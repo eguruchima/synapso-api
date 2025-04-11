77662c09df83f526b7e4e94934609e09c4aefd64
eguruchima <eguruchima@gmail.com>
2025-04-11 03:00:36 -0400

    gonna start user id when signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, unless: -> { request.format.json? }
   allow_browser versions: :modern
+
+  helper_method :current_user
+
+  def current_user
+    User.find_by(id: cookies.signed[:user_id])
+  end
+
+  def authenticate_user
+    unless current_user
+      render json: {}, status: :unauthorized
+    end
+  end
 end
