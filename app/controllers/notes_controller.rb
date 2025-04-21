eb94212a0d9e9ba8a28ae90e53aa44ff80816026
eguruchima <eguruchima@gmail.com>
2025-04-21 19:31:25 -0400

    added create and show functions and tests, everything is passing well

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,4 +3,18 @@ class NotesController < ApplicationController
     @notes = Note.all
     render :index
   end
+
+  def show
+    @note = Note.find(params[:id])
+    render :show
+  end
+
+  def create
+    @note = Note.create(
+      title: params[:title],
+      content: params[:content],
+      user_id: params[:user_id]
+    )
+    render :show
+  end
 end
--- test/controllers/notes_controller_test.rb
+++ test/controllers/notes_controller_test.rb
@@ -20,4 +20,42 @@ class NotesControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Note.count, data.length
   end
+
+  test "show" do
+    user = User.create!(
+      name: "Show Tester",
+      email: "show@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+    note = Note.create!(
+      title: "Detail Note",
+      content: "Deep dive",
+      user: user
+    )
+    get "/notes/#{note.id}.json"
+    assert_response :success
+
+    data = JSON.parse(response.body)
+    expected_keys = %w[id title content user_id created_at updated_at]
+    assert_equal expected_keys.sort, data.keys.sort
+  end
+
+  test "create" do
+    user = User.create!(
+      name: "Creator",
+      email: "create@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+assert_difference "Note.count", 1 do
+  post "/notes.json", params: {
+    title: "New Note",
+    content: "Content goes here",
+    user_id: user.id
+      }
+    end
+
+  assert_response :success
+  end
 end
